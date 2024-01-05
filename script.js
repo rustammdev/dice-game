8ab73ea5f905cb9a076e1938032e9439bb1dce7e
Rustamjon Nuriddinov <rustamcheek11@gmail.com>
2024-01-05 22:30:18 +0500

    Rustam Nuriddinov

--- script.js
+++ script.js
@@ -14,22 +14,68 @@ const player1 = document.querySelector('.player--1')
 const current0 = document.querySelector('#current--0')
 const current1 = document.querySelector('#current--1')
 
+// players score
+// hold btn bosilgandagi qiymat
+const score0 = document.querySelector('#score--0')
+const score1 = document.querySelector('#score--1')
+
+// modal
+const modal = document.querySelector('#overlay')
+
+// winner
+const winnerPlayer = document.querySelector('#winner')
+
+// restart btn
+const btnReset = document.querySelector('#restartGame')
+
 
 // btn roll dice
 btnRoll.addEventListener('click', () => {
     let rand = randomImg()
     changeActive(rand);
 
-
     if (activPlayer(player0)){
         current1.textContent = 0
         let cr0 = Number(current0.textContent)
-        current0.textContent  = cr0 + rand
+        if (!(rand == 1)){
+            current0.textContent  = cr0 + rand
+        }
     }else {
         current0.textContent = 0
         let cr1 = Number(current1.textContent)
-        current1.textContent  = cr1 + rand
+        if (!(rand == 1)){
+            current1.textContent  = cr1 + rand
+        }
     }
+})
+
+// btn hold
+btnHold.addEventListener('click', () => {
+    let cr0 = Number(current0.textContent)
+    let cr1 = Number(current1.textContent)
+    let sc0 = Number(score0.textContent)
+    let sc1 = Number(score1.textContent)
+    
+    if (activPlayer(player0)){
+        score0.textContent = cr0 + sc0
+    }else {
+        score1.textContent = cr1 + sc1
+    }
+    gameOver(Number(score0.textContent), Number(score1.textContent));
+    current1.textContent = 0
+    current0.textContent = 0
+    changeActive(1)
+
+})
+
+// restart game
+btnReset.addEventListener('click', () => {
+    restart();
+})
+
+// new game
+btnNew.addEventListener('click', () => {
+    restart();
 })
 
 // random img
@@ -53,4 +99,30 @@ function changeActive(params) {
         player0.classList.toggle('player--active')
         player1.classList.toggle('player--active')
     }
+}
+
+// game over
+function gameOver(param1, parma2) {
+    if (param1 >= 30) {
+        winnerPlayer.textContent = 'Player 1'
+    }else {   
+        winnerPlayer.textContent = 'Player 2'
+    }
+    if (param1 >= 30 || parma2 >= 30){
+        btnHold.disabled = true;
+        btnRoll.disabled = true;
+
+        modal.classList.remove('hidden')
+    }
+}
+
+// restart game
+function restart() {
+    current0.textContent = 0
+    current1.textContent = 0
+
+    score0.textContent = 0
+    score1.textContent = 0
+
+    modal.classList.add('hidden')
 }
